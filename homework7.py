23219df54f530c426c123f89173f0fb34fd41756
EskiSlav <uberbug544@gmail.com>
2020-04-26 10:08:23 +0300

    Task 3 is done not very good, Task 1,2 Little mistakes. Task 4 Nice

--- homework7.py
+++ homework7.py
@@ -1,31 +1,42 @@
-# Task14
 
-# l = [str(x) + str(x+1) for x in range(100)]
-# print(l)
-# # Task2
-# l = [str(x) + str(x*7) for x in range(20)]
-# print(l)
-# 
-# Task3
-# import random
-# import time
-# l = []
-# while(1):
-# 	el = random.randint(0, 1000)
-# 	l.append(el)
-# 	m = [x * 0.621 for x in l]
-# 	print(m)
-# 	time.sleep(1)
+# -0.5 because homework is done not in time 
+
+# Task1 ( -0.1 )
+l = [ str(x) + str(x+1) for x in range(1,101)]
+print(l)
 
 
-# Task4
+# Task2 ( -0.1 )
+l = [ str(x) + str(x*7) for x in range(1, 21) ]
+print(l)
+
+# Task3 ( -0.5 )
+import random
+import time
+
+# I had to use a function
+def convert(num):
+	return num * 0.621
+
+# name your variables correctly
+kilometers = []
+miles      = [] 
+
+while(1):
+	kilometers.append(random.randint(0, 1000))
+	miles.append(convert(kilometers[-1]))
+	print('\r', miles, flush=True, end='')
+	
+	time.sleep(1)
+
+
+# Task4 ( Nice )
 file = open('numbers.txt', 'w')
-
 
 while True:
 	a = int(input())
 	if a != 0:
-		file.write(str(a))
+		file.write(str(a) + ' ') # Add space
 		print(a*2)
 	if a == 0:
 		break
